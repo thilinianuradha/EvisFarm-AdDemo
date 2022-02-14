030779b066fedf8c8f7e0964f408446bad2b6289
thilini-anuradha <pttanuradha@std.appsc.sab.ac.lk>
2022-02-14 12:55:29 +0530

    New models added

---
+++ AdDemo.Models/AcceptedRequests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdDemo.Models
+{
+    public class AcceptedRequests
+    {
+        public int Id { get; set; }
+        public string Postedby { get; set; }
+        public DateTime PostedOn { get; set; }
+        public bool IsAvailable { get; set; }
+        public string AcceptedBy { get; set; }
+        public DateTime AcceptedOn { get; set; }
+
+    }
+}
---
+++ AdDemo.Models/Pending_request.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdDemo.Models
+{
+    public class Pending_request
+    {
+        public int Id { get; set; }
+        public int AnnouncementId { get; set; }
+        public String PostedBy { get; set; }
+        public DateTime PostedOn { get; set; }
+        public bool IsAccepted { get; set; }
+        public DateTime AcceptedOn { get; set; }
+        public int No_of_requests { get; set; }
+
+
+
+
+
+
+    }
+}
