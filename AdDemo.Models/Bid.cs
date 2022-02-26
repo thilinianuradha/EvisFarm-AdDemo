38f3745ebbb4829547055f9bf909b0c100182d01
thilini-anuradha <pttanuradha@std.appsc.sab.ac.lk>
2022-02-26 13:55:41 +0530

    Add a bid model

---
+++ AdDemo.Models/Bid.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdDemo.Models
+{
+    public class Bid
+    {
+        public int Id { get; set; }
+
+    }
+}
