6e8417a7fcc2efb15f0e514e539afec736f14326
thilini-anuradha <pttanuradha@std.appsc.sab.ac.lk>
2022-03-05 22:43:38 +0530

    Bid-controller added

---
+++ AdDemo/Controllers/BidsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BidsController : ControllerBase
+    {
+    }
+}
