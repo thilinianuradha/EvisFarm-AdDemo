a37287754873f7edc4dd9be3c5cc473455329ea8
thilini-anuradha <pttanuradha@std.appsc.sab.ac.lk>
2022-03-18 11:16:19 +0530

    Cors congiguration added

--- AdDemo/Program.cs
+++ AdDemo/Program.cs
@@ -7,11 +7,25 @@ using AdDemo.Services.PendingRequests;
 using AdDemo.Services.UserProfiles;
 using AdDemo.Services.Users;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Cors config
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:3000");
+                      });
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -33,6 +47,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
